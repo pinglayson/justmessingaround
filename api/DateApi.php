4d86bee702a1f34f0a036efd2f0faea53c131bae
ping john layson <pingjohnlayson@pings-MacBook-Pro.local>
2019-09-19 11:12:44 +0930

    return diff base on requested format

--- api/DateApi.php
+++ api/DateApi.php
@@ -9,22 +9,45 @@ class DateApi extends BaseApi
 {
     public static function between()
     {
-        $return = "";
-        
-        if(isset($_GET['from']) && isset($_GET['to'])){
+        $return = '';
+
+        if (isset($_GET['from']) && isset($_GET['to']) && isset($_GET['format'])) {
 
             // http://justmessingaround.test/api/v1/between?from=2019-09-18%2006:57:44&to=2019-09-01%2006:57:44
+            // format - days|weekdays|weeks|seconds|minutes|hours|years
             $from = Carbon::parse($_GET['from']);
             $to = Carbon::parse($_GET['to']);
 
-            self::returnSuccess("Days: ". $from->diffInDays($to));
-
+            if (isset($_GET['format'])) {
+                switch ($_GET['format']) {
+                    case 'days':
+                        $return = $from->diffInDays($to);
+                        break;
+                    case 'weekdays':
+                        $return = $from->diffInWeekdays($to);
+                        break;
+                    case 'weeks':
+                        $return = $from->diffInWeeks($to);
+                        break;
+                    case 'seconds':
+                        $return = $from->diffInSeconds($to);
+                        break;
+                    case 'minutes':
+                        $return = $from->diffInMinutes($to);
+                        break;
+                    case 'hours':
+                        $return = $from->diffInHours($to);
+                        break;
+                    case 'years':
+                        $return = $from->diffInYears($to);
+                        break;
+                    default:
+                        self::returnFailed("Allowable format values: days|weekdays|weeks|seconds|minutes|hours|years");
+                }
+            }
+            self::returnSuccess($return);
         } else {
-            $return = 'Required fields: from, to ';
+            self::returnFailed('Required fields: from, to, format ');
         }
-
-        self::returnSuccess($return);
-        
     }
-
 }
