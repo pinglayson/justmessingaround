d8a2abd3651a3f4f2686977f01a210b5f0180594
ping john layson <pingjohnlayson@pings-MacBook-Pro.local>
2019-09-19 13:35:01 +0930

    timezone difference

--- api/DateApi.php
+++ api/DateApi.php
@@ -13,10 +13,17 @@ class DateApi extends BaseApi
 
         if (isset($_GET['from']) && isset($_GET['to']) && isset($_GET['format'])) {
 
-            // http://justmessingaround.test/api/v1/between?from=2019-09-18%2006:57:44&to=2019-09-01%2006:57:44
-            // format - days|weekdays|weeks|seconds|minutes|hours|years
-            $from = Carbon::parse($_GET['from']);
-            $to = Carbon::parse($_GET['to']);
+            // http://justmessingaround.test/api/v1/between?from=2019-09-18%2006:57:44&to=2019-09-01%2006:57:44&format=days&to_tz=America/Sitka
+
+            $from = self::validateDate($_GET['from']);
+            if (isset($_GET['from_tz'])) {
+                $from = self::prepareDateTimezone($_GET['from'], $_GET['from_tz']);
+            }
+
+            $to = self::validateDate($_GET['to']);
+            if (isset($_GET['to_tz'])) {
+                $to = self::prepareDateTimezone($_GET['to'], $_GET['to_tz']);
+            }
 
             if (isset($_GET['format'])) {
                 switch ($_GET['format']) {
@@ -42,12 +49,31 @@ class DateApi extends BaseApi
                         $return = $from->diffInYears($to);
                         break;
                     default:
-                        self::returnFailed("Allowable format values: days|weekdays|weeks|seconds|minutes|hours|years");
+                        self::returnFailed('Allowable format values: days|weekdays|weeks|seconds|minutes|hours|years.');
                 }
             }
             self::returnSuccess($return);
         } else {
-            self::returnFailed('Required fields: from, to, format ');
+            self::returnFailed('Required fields: from, to, format.');
+        }
+    }
+
+    private static function prepareDateTimezone($date, $timezone)
+    {
+        if (in_array($timezone, timezone_identifiers_list())) {
+            return Carbon::parse($date, $timezone);
+        } else {
+            self::returnFailed('Invalid timezone, check https://www.php.net/manual/en/timezones.php for valid timezones.');
+        }
+    }
+
+    private static function validateDate($date, $format = 'Y-m-d H:i:s')
+    {
+        $d = \DateTime::createFromFormat($format, $date);
+        if($d && $d->format($format) == $date){
+            return Carbon::parse($date);
+        } else {
+            self::returnFailed('Invalid date or format. e.g \'2019-09-18 06:57:44\'');
         }
     }
 }
